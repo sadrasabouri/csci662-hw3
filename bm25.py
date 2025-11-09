bd96f242bab7966ccaac1d0388a081d0686771f3
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-11-09 15:53:05 -0800

    done : bm25 file is done.

--- bm25.py
+++ bm25.py
@@ -6,27 +6,58 @@
  You are NOT required to do this from scratch
  You are allowed to use libraries like retriv
 """
+import json
+from retriv import SearchEngine
 from RetrievalModel import *
 
 class BM25(RetrievalModel):
-    def __init__(self, model_file, b=.75, k=1.2):
+    def __init__(self, model_file, b=.75, k=1.2, parameters={}):
+        self.model_file = model_file
         self.b = b
         self.k = k
-        self.token2id = {}
-        self.doc_ids = {} 
-        # TODO add more dicts as needed to store frequencies/scores
-        self.avg_num_words_per_doc = None
+        self.min_df = parameters.get('min_df', 1)
+        self.tokenizer = parameters.get('tokenizer', 'whitespace')
+        self.stemmer = parameters.get('stemmer', 'english')
+        self.stopwords = parameters.get('stopwords', 'english')
+        self.do_lowercasing = parameters.get('do_lowercasing', True)
+        self.do_ampersand_normalization = parameters.get('do_ampersand_normalization', True)
+        self.do_acronyms_normalization = parameters.get('do_acronyms_normalization', True)
+        self.do_punctuation_removal = parameters.get('do_punctuation_removal', True)
+        self.do_special_chars_normalization = parameters.get('do_special_chars_normalization', True)
+        self.index_file = f"{model_file}_b{self.b}_k{self.k}_mdf{self.min_df}_tkn{self.tokenizer}_stm{self.stemmer}_sw{self.stopwords}_lc{self.do_lowercasing}_an{self.do_ampersand_normalization}_acn{self.do_acronyms_normalization}_pr{self.do_punctuation_removal}_scn{self.do_special_chars_normalization}"
         super().__init__(model_file)
 
 
-    def index(self, input_file):
+    def index(self, input_file, use_cache=True):
         """
         This method is used to train your models and generated for a given input_file a trained model
         :param input_file: path to training file with a text and a label per each line
+        :param use_cache: wether to use cached model
         """
         ## TODO write your code here to calculate term_doc_freqs and relative_doc_lens, 
-        
+        # Sadra: will use the `retriv` instead
+        if use_cache:
+            try:
+                self = self.load_model()
+                return
+            except FileNotFoundError:
+                pass
+        with open(input_file, 'r') as f:
+            data = json.load(f)
+            se = SearchEngine(index_name=self.index_file,
+                        model="bm25",
+                        min_df=self.min_df, tokenizer=self.tokenizer,
+                        stemmer=self.stemmer, stopwords=self.stopwords,
+                        do_lowercasing=self.do_lowercasing,
+                        do_ampersand_normalization=self.do_ampersand_normalization,
+                        do_acronyms_normalization=self.do_acronyms_normalization,
+                        do_punctuation_removal=self.do_punctuation_removal,
+                        do_special_chars_normalization=self.do_special_chars_normalization,
+                        hyperparams={'b': self.b, 'k1': self.k}
+                        ).index(data)
+            se.save()
         # then cache the class object using `self.save_model`
+        self.save_model()
 
 
     def search(self, query, k):
@@ -38,8 +69,5 @@ class BM25(RetrievalModel):
         :return: predictions list
         """
         ## TODO write your code here (and change return)
-
-
-
-
-
+        # Sadra: will use the `retriv` instead
+        return SearchEngine.load(self.index_file).search(query=query, cutoff=k)
