a0ef36210c2ef2b2e0ef33c8e8ad5b111d58bf51
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-11-09 15:51:40 -0800

    add : arguments added to the index.

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
+import os
 import argparse
 from bm25 import *
-
 
 def get_arguments():
     # Please do not change the naming of these command line options or delete them. You may add other options for other hyperparameters but please provide with that the default values you used
@@ -8,7 +8,17 @@ def get_arguments():
     parser.add_argument("-m", default="bm25", help="retriever model: what retriever to use")
     parser.add_argument("-i",  help="input file: the name/path of texts to index")
     parser.add_argument("-o", help="index name: the name/path to save index to disk")
-   
+    parser.add_argument("--bm25-b", help="BM25 b hyperparameter", type=float, default=0.75)
+    parser.add_argument("--bm25-k", help="BM25 k hyperparameter", type=float, default=1.2)
+    parser.add_argument("--min-df", help="minimum document frequency", type=int, default=1)
+    parser.add_argument("--tokenizer", help="tokenizer to use", type=str, default='whitespace')
+    parser.add_argument("--stemmer", help="stemmer to use", type=str, default='english')
+    parser.add_argument("--stopwords", help="stopwords to use", type=str, default='english')
+    parser.add_argument("--do-lowercasing", help="whether to do lowercasing", type=bool, default=True)
+    parser.add_argument("--do-ampersand-normalization", help="whether to do ampersand normalization", type=bool, default=True)
+    parser.add_argument("--do-acronyms-normalization", help="whether to do acronyms normalization", type=bool, default=True)
+    parser.add_argument("--do-punctuation-removal", help="whether to do punctuation removal", type=bool, default=True)
+    parser.add_argument("--do-special-chars-normalization", help="whether to do special characters normalization", type=bool, default=True)
 
     return parser.parse_args()
 
@@ -17,7 +27,17 @@ if __name__ == "__main__":
     args = get_arguments()
 
     if "bm25" in args.m:
-        model = BM25(model_file=args.o)
+        model = BM25(model_file=args.o, b=args.bm25_b, k=args.bm25_k, parameters={
+            'min_df': args.min_df,
+            'tokenizer': args.tokenizer,
+            'stemmer': args.stemmer,
+            'stopwords': args.stopwords,
+            'do_lowercasing': args.do_lowercasing,
+            'do_ampersand_normalization': args.do_ampersand_normalization,
+            'do_acronyms_normalization': args.do_acronyms_normalization,
+            'do_punctuation_removal': args.do_punctuation_removal,
+            'do_special_chars_normalization': args.do_special_chars_normalization
+        })
     else:
         ## TODO add at least one other retriever
         model = None
