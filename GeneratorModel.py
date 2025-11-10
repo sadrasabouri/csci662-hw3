8ea10c0031ddf21ad97d402810fad3febc202989
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-11-09 16:35:31 -0800

    update : prompts updated.

--- GeneratorModel.py
+++ GeneratorModel.py
@@ -1,17 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
 # TODO define prompt(s)
-PROMPT_WO_DOCS = """
-Answer the following question:
-Q: {question}
+PROMPT_WO_DOCS = """answer the following question:
+<q> {question} </q>
+Wrap your final answer in <answer> [ANSWER] </answer> tags.
 """
 PROMPT_W_DOCS = """
 Based on the following text:
-<text>
 {retrieved_documents}
-</text>
-{PROMPT_WO_DOCS}
-"""
+Use document id (e.g., <doc_0>) to refer to the content to """ + PROMPT_WO_DOCS
 
 class GeneratorModel(object, metaclass=ABCMeta):
     def __init__(self, model_file):
